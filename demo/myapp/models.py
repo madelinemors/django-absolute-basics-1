1e793ea656ee5ae945c32c942e207885dc79c2d3
madelinemors <morsmadeline@gmail.com>
2025-12-26 14:40:52 +0000

    Add database functionality

--- demo/myapp/models.py
+++ demo/myapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class TodoItem(models.Model):
+    title = models.CharField(max_length=200) # python attribute - properties of the db
+    completed = models.BooleanField(default=False)
